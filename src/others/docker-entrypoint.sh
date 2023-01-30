25caf8e0cc93c2d99faf7cdedf5837bb523b0d99
Dsobh <davidsobrin@gmail.com>
2023-01-30 13:47:53 +0100

    Bug with entrypoint and roslaunch fixed

--- src/others/docker-entrypoint.sh
+++ src/others/docker-entrypoint.sh
@@ -4,4 +4,4 @@ source /opt/ros/humble/setup.bash
 source /root/ros2_ws/install/setup.bash
 source /root/catkin_ws/devel/setup.bash
 
-ros2 launch ros1_bridges_launch.py
+ros2 launch ros1_bridge ros1_bridges_launch.py
