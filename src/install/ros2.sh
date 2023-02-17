77d0a2a2703bbc1a33798d30d3595251e3d3e95c
dsobh <davidsobrin@gmail.com>
2023-02-17 11:21:36 +0100

    cyclone y fastrtps added to the installation

--- src/install/ros2.sh
+++ src/install/ros2.sh
@@ -4,6 +4,9 @@ curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | tee /etc/apt/sources.list.d/ros2.list > /dev/null
 apt update
 apt install -y ros-humble-ros-base
+
+apt install -y ros-humble-rmw-cyclonedds-cpp ros-humble-rmw-fastrtps-cpp ros-humble-cyclonedds ros-humble-fastrtps
+
 rosdep init
 rosdep update
 
