632f470ebd1696da9109c0ff93885ed2ded1f15a
dsobh <davidsobrin@gmail.com>
2023-06-28 13:06:52 +0200

    docker-entrypoint changed to solve segmentation fault problem in moveit bridge

--- src/others/docker-entrypoint.sh
+++ src/others/docker-entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+source /root/catkin_ws/devel/setup.bash
 source /opt/ros/humble/setup.bash
 source /root/ros2_ws/install/setup.bash
-source /root/catkin_ws/devel/setup.bash
 
-ros2 launch ros1_bridge ros1_bridges_launch.py
+ros2 launch ros1_bridge ros1_bridges_launch.py &
+
+while true
+do
+  ros2 run ros1_bridge moveit_2_to_1
+done
+
